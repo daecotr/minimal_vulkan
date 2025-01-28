0a2c7b61cffa44ba5b84eab3397e6e959dc3f85f
daecotr <c.daecotr@gmail.com>
2025-01-28 16:47:07 +0200

    Add GLM to CMake project & add GLM test

--- minimal_vulkan.cc
+++ minimal_vulkan.cc
@@ -4,6 +4,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
+#include <glm/glm.hpp>
 
 namespace minvk {
 
@@ -20,20 +21,27 @@ public:
 
 int main(int argc, char *argv[]) {
   try {
+		glm::mat4 matrix; // For GLM test
+
+		// Init GLFW, handle errors
     if (!glfwInit())
       throw minvk::Exception{"Failed to initialize GLFW"};
 
     glfwSetErrorCallback([](int error, const char *description) {
       throw minvk::Exception{description};
     });
-
+	
+		// Set GLFW window params
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+		
+		// Create window
     GLFWwindow *window = glfwCreateWindow(512, 512, "", nullptr, nullptr);
     if (!window) {
       throw minvk::Exception{"Failed to create window"};
     }
 
+		// Get available Vulkan layers
 		std::cout << "Available layers" << std::endl;
     std::vector<vk::LayerProperties> availableLayers =
         vk::enumerateInstanceLayerProperties();
@@ -41,6 +49,7 @@ int main(int argc, char *argv[]) {
       std::cout << "- " << layerProperties.layerName << std::endl;
     }
 
+		// Create Vulkan instance
     vk::ApplicationInfo applicationInfo("", 1, "", 1, VK_API_VERSION_1_3);
     vk::InstanceCreateInfo instanceCreateInfo({}, &applicationInfo);
     auto extensions = glfwGetRequiredInstanceExtensions(
@@ -48,19 +57,23 @@ int main(int argc, char *argv[]) {
     instanceCreateInfo.ppEnabledExtensionNames = extensions;
     vk::Instance instance = vk::createInstance(instanceCreateInfo);
 
+		// Create Vulkan surface for GLFW window
     vk::SurfaceKHR surface{};
     auto glfw_surface = VkSurfaceKHR{surface};
     if (glfwCreateWindowSurface(VkInstance{instance}, window, nullptr,
                                 &glfw_surface) != VK_SUCCESS)
       throw minvk::Exception{"Failed to create Vulkan surface"};
 
+		// Window cycle
     for (size_t i = 0; (i < 128) && !glfwWindowShouldClose(window); ++i) {
       glfwPollEvents();
     }
 
+		// Close window & terminate GLFW
     glfwDestroyWindow(window);
     glfwTerminate();
 
+		// Destroy Vulkan instance
     instance.destroy();
     
 		std::cout << "Okay" << std::endl;
