0f104ea29161245b6671891b3412c631fa40c193
daecotr <c.daecotr@gmail.com>
2025-01-28 15:41:52 +0200

    Dockerfile for Void

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM voidlinux/voidlinux
+
+RUN mkdir -p /etc/xbps.d && \
+  echo "repository=https://repo-default.voidlinux.org/current" > /etc/xbps.d/00-repository-main.conf
+RUN xbps-install -Syu -y xbps
+RUN xbps-install -Syu
+
+RUN xbps-install -Sy \
+	mesa-vulkan-radeon vulkan-loader-devel \
+	cmake make clang pkg-config \
+	glfw-devel glm
+
+RUN xbps-install -Sy bash
+
+WORKDIR /minimal_vulkan
+
+COPY . .
+
+RUN cmake -B build && \
+	cmake --build build
+
+CMD ["./build/minimal_vulkan"]
--- minimal_vulkan.cc
+++ minimal_vulkan.cc
@@ -5,7 +5,47 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
 
+namespace minvk {
+
+class Exception : public std::exception {
+protected:
+  std::string message;
+
+public:
+  explicit Exception(const std::string &message) : message{message} {}
+  const char *what() const noexcept override { return message.c_str(); }
+};
+
+} // namespace minvk
+
 int main(int argc, char *argv[]) {
-	std::cout << argv[0] << std::endl;
-	return EXIT_SUCCESS;
+  try {
+    if (!glfwInit())
+      throw minvk::Exception{"Failed to initialize GLFW"};
+
+    glfwSetErrorCallback([](int error, const char *description) {
+      throw minvk::Exception{description};
+    });
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+    GLFWwindow *window = glfwCreateWindow(512, 512, "", nullptr, nullptr);
+    if (!window) {
+      throw minvk::Exception{"Failed to create window"};
+    }
+
+    for (size_t i = 0; (i < 128) && !glfwWindowShouldClose(window); ++i) {
+      glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+		std::cout << "Okay" << std::endl;
+  } catch (minvk::Exception err) {
+    std::cout << "minvk::Exception: " << err.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
